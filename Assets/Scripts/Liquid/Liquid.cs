e79d9862db45d5c895fdb0d3a511db132a17c89c
CalBolt <calbolt22@gmail.com>
2019-09-18 17:59:07 -0700

    Fixed liquid composition

--- Assets/Scripts/Liquid/Liquid.cs
+++ Assets/Scripts/Liquid/Liquid.cs
@@ -7,10 +7,13 @@ public class Liquid : MonoBehaviour
     // Inspector provides percentage
     [Range(0, 100)]
     [SerializeField] public float red;
+    private float lastRed;
     [Range(0, 100)]
     [SerializeField] public float green;
+    private float lastGreen;
     [Range(0, 100)]
     [SerializeField] public float blue;
+    private float lastBlue;
 
     private Renderer _renderer;
 
@@ -20,13 +23,35 @@ public class Liquid : MonoBehaviour
         if (float.IsNaN(green)) green = 33.33f;
         if (float.IsNaN(blue)) blue = 33.33f;
         float total = red + green + blue;
-        if (total != 100 && total != 0)
+        
+        if(lastRed != red)
         {
-            red = 100 * (red / total);
-            Debug.Log("red" + total);
-            green = 100 * (green / total);
-            blue = 100 * (blue / total);
+            float split = 100 - red;
+            float greenProp = green / (green + blue);
+            float blueProp = blue / (green + blue);
+            green = greenProp * split;
+            blue = blueProp * split;
         }
+        else if(lastGreen != green)
+        {
+            float split = 100 - green;
+            float redProp = red / (red + blue);
+            float blueProp = blue / (red + blue);
+            red = redProp * split;
+            blue = blueProp * split;
+        }
+        else if (lastBlue != blue)
+        {
+            float split = 100 - blue;
+            float redProp = red / (red + green);
+            float greenProp = green / (red + green);
+            red = redProp * split;
+            green = greenProp * split;
+        }
+
+        lastRed = red;
+        lastGreen = green;
+        lastBlue = blue;
     }
 
     private void Start()
