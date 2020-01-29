0b72170eb0da224b083067abe2c2b11b3c90ad25
Alex Chiu <archiu@ucdavis.edu>
2020-01-28 16:06:32 -0800

    commit Jan 28 1

--- Assets/Scripts/RangeScriptableObject.cs
+++ Assets/Scripts/RangeScriptableObject.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/RangeScriptableObject", order = 1)]
 public class RangeScriptableObject : ScriptableObject
 {
-	[SerializeField] private string need_name;
+	private enum Needs{ Terrain,RedLiquid,BlueLiquid,GreenLiquid,BlackLiquid,GasX,GasY,GasZ,Temperature,Lighting};
+	[SerializeField] private Needs need;
     [SerializeField] private float weight;
     [SerializeField] private float[] ranges = new float[4];
-    public string getName(){ return need_name; }
+    public int getName(){ return (int)need; }
     public float getWeight(){ return weight; }
     public float[] getRanges(){ return ranges; }
 }
