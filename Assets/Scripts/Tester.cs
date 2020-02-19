ddab1c8a174bc5a0af6d3fedf74d8c85ecc44c86
Alex Chiu <archiu@ucdavis.edu>
2020-02-19 12:46:11 -0800

    Feb 19

---
+++ Assets/Scripts/Tester.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Tester : MonoBehaviour
+{
+    public TileRetriever toTest;
+    public int rad;
+    int ct = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // detects the environment every second
+        InvokeRepeating("UpdatePosition", 0, 1);
+    }
+
+    // detects the environment every second
+    void UpdatePosition()
+    {
+        ct++;
+        List<TileBase> ts = toTest.GetTiles(transform.position, rad);
+        for (int i = 0; i < ts.Count; i++)
+        {
+            print("iter " + ct + ": " + ts[i].name);
+        }
+    }
+}
