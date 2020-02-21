f9468b1511a517a1dc4c344cfd0caa3d8ca3bfe0
Tehya <jinjurhee@gmail.com>
2020-02-21 12:03:27 -0800

    Just the very beginning. Working more today soon.

---
+++ Assets/Scripts/FoodSourceManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSourceManager : MonoBehaviour
+{
+    //array of all active food source instances
+    public FoodSource[] foodSources;
+    
+    //Should we have instantiation functions for different food sources? and how to organize it all --maybe a table
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
