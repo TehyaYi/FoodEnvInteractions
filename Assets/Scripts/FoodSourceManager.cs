1fcb66d17a9626d7606ce20cf2124f2e7ab6c5e5
Tehya <jinjurhee@gmail.com>
2020-02-23 17:40:03 -0800

    start?

--- Assets/Scripts/FoodSourceManager.cs
+++ Assets/Scripts/FoodSourceManager.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class FoodSourceManager : MonoBehaviour
 {
-    //array of all active food source instances
-    public FoodSource[] foodSources;
-    
+    //Food source manager will be keeping references to the food sources and telling the food dis. sys 
+    //and environmental interactions sys. to update when they need to
+
+    //dictionary(?) of all active food source instances
+    private IDictionary<int, FoodSource> foodSourceDict = new Dictionary<int, FoodSource>();
+    private int currIndex= 0;
+
     //Should we have instantiation functions for different food sources? and how to organize it all --maybe a table
+
+    public int add(FoodSource newFoodSource) {
+        currIndex++;
+        foodSourceDict.Add(currIndex, newFoodSource);
+        // TODO : tell food dist and food env to update
+        return currIndex;
+    }
+
+    public void delete(int currIndex)
+    {
+        foodSourceDict.Remove(currIndex);
+        // TODO : tell food dist and food env to update
+    }
 
     // Start is called before the first frame update
     void Start()
